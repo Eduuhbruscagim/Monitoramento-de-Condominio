c8d4d289b0eaf4624652f413e1ae0cc0687d824d
Eduardo Bruscagim <eduguilhermebruscagim@gmail.com>
2026-01-19 02:50:02 -0300

    fix: corrigir carregamento de variáveis de ambiente (.env)

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { resolve } from 'path';
 
 export default defineConfig({
   root: 'src',
+  envDir: '../', // ✅ Faz o Vite procurar .env na raiz do projeto
   
   build: {
     outDir: '../dist',
